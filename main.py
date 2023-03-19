a869338cf59e18a3d73c0634f301aafa97defb43
Piotr <ptk.siewiera@gmail.com>
2023-03-19 17:27:52 +0100

    Commit

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ def read_file():
         maze.append([x.strip('\n') for x in line if x != '\n'])
     return maze
 
+def find_ids(maze):
+    y = 0
+    for row in maze:
+        x = 0
+        for col in row:
+            if col == 's':
+                return y, x
+            x += 1
+        y += 1
 
 maze = []
 maze = read_file()
@@ -14,3 +23,9 @@ for i in maze:
 
 open_list = []
 closed_list = []
+print()
+row, col = find_ids(maze)
+print(row)
+print(col)
+print(maze[row][col])
+closed_list.append([row,col,0])
